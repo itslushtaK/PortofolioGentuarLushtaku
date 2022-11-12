0689f6fe34b5053fb708924cbd9e151236c74e2d
Gentuar Lushtaku <93317180+itslushtaK@users.noreply.github.com>
2022-11-12 17:57:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-var content = document.getElementsByTagName('body')[0];
-var dark= document.getElementById('myTopnav');
-var darkmode = document.getElementById('dark-change');
-darkmode.addEventListener('click' ,function(){
-  content.classList.toggle('night');
-  dark.classList.toggle('nights');
-});
-
-
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -15,13 +6,4 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 
